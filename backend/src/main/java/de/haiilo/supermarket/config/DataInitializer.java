5c4c9416810133c7d81e339a9b8fec9d0e0e2a7e
mmansurov <mmansurov@outlook.com>
2025-03-31 16:54:23 +0200

    DataInitializer, CORS, SecurityConfig

---
+++ backend/src/main/java/de/haiilo/supermarket/config/DataInitializer.java
@@ -0,0 +1,78 @@
+package de.haiilo.supermarket.config;
+
+import de.haiilo.supermarket.domain.Item;
+import de.haiilo.supermarket.domain.Offer;
+import de.haiilo.supermarket.domain.Price;
+import de.haiilo.supermarket.repository.ItemRepository;
+import de.haiilo.supermarket.repository.OfferRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataInitializer {
+
+    @Bean
+    public CommandLineRunner initData(ItemRepository itemRepository, OfferRepository offerRepository) {
+        return args -> {
+            // Apple
+            var apple = new Item();
+            apple.setName("Apple");
+
+            var applePrice = new Price();
+            applePrice.setValue(30);
+            applePrice.setItem(apple);
+            apple.setCurrentPrice(applePrice);
+            apple.getPrices().add(applePrice);
+            apple = itemRepository.save(apple);
+
+            var appleOffer = new Offer();
+            appleOffer.setQuantity(2);
+            appleOffer.setDiscountPercentage(25); // 25% off when buying 2
+            appleOffer.setItem(apple);
+            appleOffer = offerRepository.save(appleOffer);
+            apple.setCurrentOffer(appleOffer);
+            apple.getOffers().add(appleOffer);
+
+            // TODO Calculation logic
+            Integer appleOfferPrice = 45;
+            applePrice.setCalculatedOfferPrice(appleOfferPrice);
+            itemRepository.save(apple);
+
+            // Banana
+            var banana = new Item();
+            banana.setName("Banana");
+
+            var bananaPrice = new Price();
+            bananaPrice.setValue(50);
+            bananaPrice.setItem(banana);
+            banana.setCurrentPrice(bananaPrice);
+            banana.getPrices().add(bananaPrice);
+            banana = itemRepository.save(banana);
+
+            var bananaOffer = new Offer();
+            bananaOffer.setQuantity(3);
+            bananaOffer.setDiscountPercentage(15);  // 15% off when buying 3
+            bananaOffer.setItem(banana);
+            bananaOffer = offerRepository.save(bananaOffer);
+            banana.setCurrentOffer(bananaOffer);
+            banana.getOffers().add(bananaOffer);
+
+            // TODO Calculation logic
+            Integer bananaOfferPrice = 130;
+            bananaPrice.setCalculatedOfferPrice(bananaOfferPrice);
+            itemRepository.save(banana);
+
+            // Peach
+            var peach = new Item();
+            peach.setName("Peach");
+
+            var peachPrice = new Price();
+            peachPrice.setValue(60);
+            peachPrice.setItem(peach);
+            peach.setCurrentPrice(peachPrice);
+            peach.getPrices().add(peachPrice);
+            itemRepository.save(peach);
+        };
+    }
+}
